98f3d5ba5393d19214cf41ff3db4c79888863fc2
Lucas Pazzim <lpazzim@gmail.com>
2021-05-05 13:18:42 -0300

    API integration

--- next.config.js
+++ next.config.js
@@ -1,8 +1,15 @@
+const withPlugins = require('next-compose-plugins');
+const optimizedImages = require('next-optimized-images');
+
+// module.exports = withPlugins([
+//   optimizedImages,
+// ]);
+
 module.exports ={
   images:{
     domains: ['storage.googleapis.com']
   },
   future: {
     webpack5: true,
-  },
+  }
 };
